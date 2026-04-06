802dd2d4b365f778f3626850eb2fa03c56d84c6c
ayushhingnekar <ayush786hingnekar@gmail.com>
2026-04-06 17:47:51 +0530

    added new files and deleted old files

---
+++ 02_Node-Practice/PathModule.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const file = "text/peter.txt"
+
+// console.log(path.extname(file));
+// console.log(path.dirname(file));
+// console.log(path.basename(file));
+
+// console.log(path.resolve("text", "peter.txt"));                 //Getting root path from this command
+// console.log(path.isAbsolute(file));
+
+console.log(__dirname);                                            // To know the directory name    | Both are global constant
+console.log(__filename);                                           // TO know the file name         | Both are global constant
+
+
+
