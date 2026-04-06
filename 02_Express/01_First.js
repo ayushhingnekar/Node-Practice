0d438a6163bbe5c271bbd56d76f9541ae619aa24
ayushhingnekar <ayush786hingnekar@gmail.com>
2026-04-06 19:24:19 +0530

    Started express js

---
+++ 02_Express/01_First.js
@@ -0,0 +1,19 @@
+const express = require('express');
+                                                                    // Express does not overrides the same route it keeps first route
+// const express = require('express')()                             // Can write this type also if don't have to write next line
+
+const app = express();                                              // Can give any name instead of app
+
+app.get("",(req, resp)=>{                                           // This is Home Page
+    resp.send("<h1>This is first Expressjs Program</h1>")
+})
+
+app.get("/about",(req, resp)=>{
+    resp.send("<h1>This is about page</h1>")
+})
+
+app.get("/contact",(req, resp)=>{
+    resp.send("<h1>This is contact page</h1>")
+})
+
+app.listen(5000)
