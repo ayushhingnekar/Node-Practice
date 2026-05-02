2dfc8373ea618b9f01b0c9100b8df8ed162c7aae
ayushhingnekar <ayush786hingnekar@gmail.com>
2026-05-02 23:35:08 +0530

    Learn Embeddding

---
+++ 03_MongoDB/07_Embedding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Embedding() {
+  return (
+    <div>07_Embedding</div>
+  )
+}
+
+export default Embedding
---
+++ 03_MongoDB/08_Refrencing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Refrencing() {
+  return (
+    <div>08_Refrencing</div>
+  )
+}
+
+export default Refrencing
