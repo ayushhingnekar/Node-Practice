d7d14a8db399825ab5948cf4eb622590f3597342
ayushhingnekar <ayush786hingnekar@gmail.com>
2026-05-03 23:56:16 +0530

    Added new new files

---
+++ 03_MongoDB/Querying.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Querying() {
+  return (
+    <div>This is Querying</div>
+  )
+}
+
+export default Querying
---
+++ 03_MongoDB/Sorting.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sorting() {
+  return (
+    <div>This is Sorting</div>
+  )
+}
+
+export default Sorting
